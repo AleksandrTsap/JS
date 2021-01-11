aa944f5d70f1264ea106a3d4bfd5571073ecfc21
Aleksandr <tsap97@gmail.com>
2021-01-12 01:36:47 +0300

    исправил замечания

--- lesson4/hw2.js
+++ lesson4/hw2.js
@@ -1,34 +1,32 @@
 'use strict'
 
 
-const id1 = {
-    name: 'Масло Mobil 1',
-    price: 2500,
-    quantity: 1
+const basket = {
+    products: [
+        {
+            name: 'Масло Mobil 1',
+            price: 2500,
+            quantity: 1
+        },
+        {
+            name: 'Маслянный фильтр Subaru',
+            price: 5000,
+            quantity: 1
+        },
+        {
+            name: 'Воздушный фильтр Mahle',
+            price: 600,
+            quantity: 1
+        },
+    ],
+
+
+    getTotalPrice() {
+        return this.products.reduce((accum, current) =>
+            accum + (current.price * current.quantity), 0);
+    },
+
+
 }
 
-const id2 = {
-    name: 'Маслянный фильтр Subaru',
-    price: 500,
-    quantity: 1
-}
-
-const id3 = {
-    name: 'Воздушный фильтр Mahle',
-    price: 595,
-    quantity: 1
-}
-
-
-function sumId(id) {
-    return (id.price * id.quantity)
-}
-
-
-function sumBasket(...theArgs) {
-    return theArgs.reduce((sum, current) => {
-        return sum + current;
-    }, 0);
-}
-
-console.log(sumBasket(sumId(id3), sumId(id2), sumId(id1)))
+console.log(basket.getTotalPrice())
